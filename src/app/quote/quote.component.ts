3ac2cf3b963a9b1fcf8562e3acc173ede3dfbb68
Roy Chela <roychela@gmail.com>
2019-06-02 18:34:17 +0300

    Add a quote details component to test the display functionality of the quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from './../quote';
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-  quotes = [];
+  quotes = [new Quote(1, 'Roy', 'Legacy is greater than currency', 'Gary Vaynerchuk') ];
 
   constructor() { }
 
