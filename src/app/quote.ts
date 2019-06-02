617431ab3b1435973852dba9d054b60c5901858a
Roy Chela <roychela@gmail.com>
2019-06-02 19:47:41 +0300

    Add delete quote functionality

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,5 +1,4 @@
 export class Quote {
   constructor(public id: number, public name: string, public description: string, public author: string) {
-
   }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Quote } from './../quote';
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
@@ -7,7 +7,14 @@ import { Quote } from './../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes = [new Quote(1, 'Roy', 'Legacy is greater than currency', 'Gary Vaynerchuk') ];
-
+  deleteQuote(isComplete, index) {
+    if (isComplete) {
+        let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}`);
+          if (toDelete) {
+            this.quotes.splice(index, 1);
+        }
+    }
+}
   constructor() { }
 
   ngOnInit() {
