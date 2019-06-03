130261e865ddcc9f37ad4a65513e9f74936b54df
Roy Chela <roychela@gmail.com>
2019-06-04 00:27:21 +0300

    Auto-generated commit

--- src/app/quote.ts
+++
@@ -1,4 +0,0 @@
-export class Quote {
-  constructor(public id: number, public name: string, public description: string, public author: string, public dateCreated: Date, public upVote: number, public downVote: number) {
-  }
-}
