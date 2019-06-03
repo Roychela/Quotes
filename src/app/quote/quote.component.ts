04af2bb3da07e5d23fc2e7ccb140fe8f164041cf
Roy Chela <roychela@gmail.com>
2019-06-03 07:37:55 +0300

    Add post quote feature functionality

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -7,6 +7,12 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes = [new Quote(1, 'Roy', 'Legacy is greater than currency', 'Gary Vaynerchuk', new Date()) ];
+  addNewQuote(quote) {
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength + 1;
+    quote.completeDate = new Date(quote.dateCreated);
+    this.quotes.push(quote);
+}
   deleteQuote(isComplete, index) {
     if (isComplete) {
         let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}`);
